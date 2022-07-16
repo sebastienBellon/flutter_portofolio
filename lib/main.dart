7b67193f1a0637d3fca1fba60794803b67607048
Sébastien Bellon <seb.bellon@protonmail.com>
2022-07-16 17:20:58 +0200

    ♻️ add a detail project page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:portofolio/page/home_page.dart';
+import 'package:portofolio/page/home/home_page.dart';
 
 void main() {
   runApp(const MyApp());
--- lib/widgets/project_card/project_card.dart
+++ lib/widgets/project_card/project_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:portofolio/page/project/project_detail.dart';
 
 import '../../models/project.dart';
 
@@ -8,67 +9,70 @@ class ProjectCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 350.0,
-      width: MediaQuery.of(context).size.width / 1.3,
-      padding: const EdgeInsets.all(15.0),
-      margin: const EdgeInsets.only(top: 15.0, right: 25.0, bottom: 25.0),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(15.0),
-        boxShadow: [
-          BoxShadow(
-            offset: const Offset(5, 15),
-            color: Colors.black.withOpacity(.15),
-            blurRadius: 15,
-          )
-        ],
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                project.name,
-                style: const TextStyle(fontSize: 18.0),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.circular(15.0),
+    return GestureDetector(
+      onTap: () => Navigator.push(
+          context, MaterialPageRoute(builder: (_) => const ProjectDetail())),
+      child: Container(
+        width: MediaQuery.of(context).size.width / 1.3,
+        padding: const EdgeInsets.all(15.0),
+        margin: const EdgeInsets.only(top: 15.0, right: 25.0, bottom: 25.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(15.0),
+          boxShadow: [
+            BoxShadow(
+              offset: const Offset(5, 15),
+              color: Colors.black.withOpacity(.15),
+              blurRadius: 15,
+            )
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  project.name,
+                  style: const TextStyle(fontSize: 18.0),
                 ),
-                padding: const EdgeInsets.all(5.0),
-                child: Text(
-                  project.year.toString(),
-                  style: const TextStyle(color: Colors.white, fontSize: 14.0),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  padding: const EdgeInsets.all(5.0),
+                  child: Text(
+                    project.year.toString(),
+                    style: const TextStyle(color: Colors.white, fontSize: 14.0),
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(height: 15.0),
+            Expanded(
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15.0),
+                child: Image.network(
+                  project.imageUrl,
+                  fit: BoxFit.cover,
                 ),
-              )
-            ],
-          ),
-          const SizedBox(height: 15.0),
-          Expanded(
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(15.0),
-              child: Image.network(
-                project.imageUrl,
-                fit: BoxFit.cover,
               ),
             ),
-          ),
-          const SizedBox(
-            height: 10.0,
-          ),
-          Text(
-            project.description,
-            maxLines: 3,
-            style: const TextStyle(
-              fontSize: 14.0,
-              overflow: TextOverflow.ellipsis,
+            const SizedBox(
+              height: 10.0,
             ),
-          )
-        ],
+            Text(
+              project.description,
+              maxLines: 3,
+              style: const TextStyle(
+                fontSize: 14.0,
+                overflow: TextOverflow.ellipsis,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
